6c8850261ad6a90b319fb929b6b6058bb70d7b64
ncpa0cpl <sbretner@touchbistro.com>
2022-12-06 10:45:09 +0100

    feat: prop comparison for style records

--- src/gjs-elements/utils/diff-props.ts
+++ src/gjs-elements/utils/diff-props.ts
@@ -1,7 +1,7 @@
 import type { DiffedProps } from "./element-extenders/map-properties";
 import { UnsetProp } from "./element-extenders/map-properties";
 
-const compareMargins = (oldArray?: any[], newArray?: any[]) => {
+const compareFlatArrays = (oldArray?: any[], newArray?: any[]) => {
   if (typeof oldArray !== typeof newArray) {
     return true;
   }
@@ -21,6 +21,34 @@ const compareMargins = (oldArray?: any[], newArray?: any[]) => {
   return false;
 };
 
+const compareFlatRecords = (
+  oldStyle: undefined | Record<string, string>,
+  newStyle: undefined | Record<string, string>
+) => {
+  if (typeof oldStyle !== typeof newStyle) {
+    return true;
+  }
+
+  if (oldStyle == undefined) return false;
+
+  const oldStyleKeys = Object.keys(oldStyle);
+  const newStyleKeys = Object.keys(newStyle!);
+
+  if (oldStyleKeys.length !== newStyleKeys.length) {
+    return true;
+  }
+
+  for (let i = 0; i < oldStyleKeys.length; i++) {
+    const key = oldStyleKeys[i];
+
+    if (oldStyle[key] !== newStyle![key]) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 export const diffProps = (oldProps: any, newProps: any, gjsElem: boolean) => {
   const diffedProps: DiffedProps = [];
 
@@ -29,15 +57,23 @@ export const diffProps = (oldProps: any, newProps: any, gjsElem: boolean) => {
 
   for (let i = 0; i < newPropsKeys.length; i++) {
     const key = newPropsKeys[i];
-
-    if (gjsElem && key === "margin") {
-      if (compareMargins(oldProps[key], newProps[key])) {
-        diffedProps.push([key, newProps[key]]);
-      }
-      continue;
+    if (gjsElem) {
       // we don't want to compare margins by reference, since
       // those can be tuples of numbers, and even if margin values
       // did not change, the tuple reference will be different
+      if (key === "margin") {
+        if (compareFlatArrays(oldProps[key], newProps[key])) {
+          diffedProps.push([key, newProps[key]]);
+        }
+        continue;
+      }
+
+      if (key === "style") {
+        if (compareFlatRecords(oldProps[key], newProps[key])) {
+          diffedProps.push([key, newProps[key]]);
+        }
+        continue;
+      }
     }
 
     if (newProps[key] !== oldProps[key]) {
@@ -48,7 +84,7 @@ export const diffProps = (oldProps: any, newProps: any, gjsElem: boolean) => {
   for (let i = 0; i < oldPropsKeys.length; i++) {
     const key = oldPropsKeys[i];
 
-    if (!(key in newProps)) {
+    if (!newPropsKeys.includes(key)) {
       diffedProps.push([key, UnsetProp]);
     }
   }
