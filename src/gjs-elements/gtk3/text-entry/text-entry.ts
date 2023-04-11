c4fe69cbf3b450d154cdc98ad84ccd9b1e5e3d30
ncpa0cpl <sbretner@touchbistro.com>
2023-04-11 16:55:46 +0200

    feat: RG 43 - added suggestions to the text input

--- src/gjs-elements/gtk3/text-entry/text-entry.ts
+++ src/gjs-elements/gtk3/text-entry/text-entry.ts
@@ -1,11 +1,12 @@
 import { DataType } from "dilswer";
+import GObject from "gi://GObject";
 import Gdk from "gi://Gdk?version=3.0";
 import Gtk from "gi://Gtk";
 import type { InputPurpose } from "../../../enums/gtk3-index";
 import type { GjsContext } from "../../../reconciler/gjs-renderer";
 import type { HostContext } from "../../../reconciler/host-context";
 import type { GjsElement } from "../../gjs-element";
-import { diffProps } from "../../utils/diff-props";
+import { compareArraysShallow, diffProps } from "../../utils/diff-props";
 import { ElementLifecycleController } from "../../utils/element-extenders/element-lifecycle-controller";
 import type { SyntheticEvent } from "../../utils/element-extenders/event-handlers";
 import { EventHandlers } from "../../utils/element-extenders/event-handlers";
@@ -48,6 +49,28 @@ export interface TextEntryProps extends TextEntryPropsMixin {
   secondaryIconTooltip?: string;
   progress?: number;
   truncateMultilinePaste?: boolean;
+  suggestions?: string[];
+  /**
+   * The minimum number of characters that must be entered before
+   * suggestions are shown.
+   *
+   * @default 1
+   */
+  suggestionMinInput?: number;
+  /**
+   * Whether the input value should be matched against the suggestions
+   * case sensitively.
+   *
+   * @default false
+   */
+  suggestionCaseSensitive?: boolean;
+  /**
+   * If false, a phrase will only be considered a match if the input
+   * value is exactly the same as the beginning of a suggestion.
+   *
+   * @default true
+   */
+  suggestionMatchAnywhere?: boolean;
   onChange?: (event: TextEntryElementEvent<{ text: string }>) => void;
   onEnter?: (event: TextEntryElementEvent) => void;
   onKeyPress?: (event: TextEntryElementEvent<Rg.KeyPressEventData>) => void;
@@ -69,6 +92,8 @@ export class TextEntryElement implements GjsElement<"TEXT_ENTRY", Gtk.Entry> {
 
   private parent: GjsElement | null = null;
 
+  private suggestionStore = new Gtk.ListStore();
+
   readonly lifecycle = new ElementLifecycleController();
   private readonly handlers = new EventHandlers<Gtk.Entry, TextEntryProps>(
     this
@@ -87,6 +112,21 @@ export class TextEntryElement implements GjsElement<"TEXT_ENTRY", Gtk.Entry> {
         .value(DataType.String, (v = "") => {
           if (this.widget.text !== v) {
             this.widget.set_text(v);
+          }
+        })
+        .suggestions(DataType.ArrayOf(DataType.String), (v) => {
+          if (v) {
+            for (const suggestion of v) {
+              this.suggestionStore.set_value(
+                this.suggestionStore.append(),
+                0,
+                suggestion
+              );
+            }
+
+            return () => {
+              this.suggestionStore.clear();
+            };
           }
         })
         .capsLockWarning(DataType.Boolean, (v = false) => {
@@ -136,6 +176,17 @@ export class TextEntryElement implements GjsElement<"TEXT_ENTRY", Gtk.Entry> {
   );
 
   constructor(props: DiffedProps) {
+    this.suggestionStore.set_column_types([GObject.TYPE_STRING]);
+    const completion = new Gtk.EntryCompletion();
+    completion.set_model(this.suggestionStore);
+    completion.set_text_column(0);
+    completion.set_match_func((_, __, iter) => {
+      const input = this.widget.text;
+      const value = this.suggestionStore.get_value(iter, 0) as string | null;
+      return !!(value && input && this.compare(input, value));
+    });
+    this.widget.set_completion(completion);
+
     this.handlers.bind("changed", "onChange", () => ({
       text: this.widget.text,
     }));
@@ -156,6 +207,32 @@ export class TextEntryElement implements GjsElement<"TEXT_ENTRY", Gtk.Entry> {
     this.updateProps(props);
 
     this.lifecycle.emitLifecycleEventAfterCreate();
+  }
+
+  private compare(input: string, suggestionValue: string): boolean {
+    const {
+      suggestionCaseSensitive = false,
+      suggestionMatchAnywhere = true,
+      suggestionMinInput,
+    } = this.propsMapper.currentProps;
+
+    let a = input;
+    let b = suggestionValue;
+
+    if (suggestionMinInput != null && input.length < suggestionMinInput) {
+      return false;
+    }
+
+    if (!suggestionCaseSensitive) {
+      a = a.toLowerCase();
+      b = b.toLowerCase();
+    }
+
+    if (suggestionMatchAnywhere) {
+      return b.includes(a);
+    }
+
+    return b.startsWith(a);
   }
 
   updateProps(props: DiffedProps): void {
@@ -237,11 +314,20 @@ export class TextEntryElement implements GjsElement<"TEXT_ENTRY", Gtk.Entry> {
     return this.propsMapper.get(key);
   }
 
+  static TextEntryPropDiffers = new Map([
+    ["suggestions", compareArraysShallow],
+  ]);
+
   diffProps(
     oldProps: Record<string, any>,
     newProps: Record<string, any>
   ): DiffedProps {
-    return diffProps(oldProps, newProps, true);
+    return diffProps(
+      oldProps,
+      newProps,
+      true,
+      TextEntryElement.TextEntryPropDiffers
+    );
   }
 
   // #endregion
