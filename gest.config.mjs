11e2e878a6d9f3c279b86625afd500fa3f4c4a21
ncpa0cpl <szymonb21@gmail.com>
2023-05-17 16:19:58 +0200

    chore: bumped gest to latest version

--- gest.config.mjs
+++ gest.config.mjs
@@ -1,11 +1,11 @@
-export default () => {
-  /** @type {import("@reactgjs/gest/config").Config} */
-  const config = {
+/** @type {import("@reactgjs/gest/config").ConfigGetter} */
+const getConfig = () => {
+  return {
     $schema: "node_modules/@reactgjs/gest/dist/gest-config.schema.json",
     testDir: "./__tests__",
     srcDir: "./src",
     setup: "./__tests__/main.setup.mjs",
   };
-
-  return config;
 };
+
+export default getConfig;
